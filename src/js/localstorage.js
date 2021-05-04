9740c713ebc7b17f55db719696a8e1e92de7daad
Robert <robertwattboolsen@gmail.com>
2021-05-04 09:55:21 +0200

    Added idea for localstorage saving

---
+++ src/js/localstorage.js
@@ -0,0 +1,9 @@
+// let localStorage = (() => {
+//     let newsStr = JSON.stringify(news)
+
+//     localStorage.setItem("news", newsStr)
+//     let local = localStorage.getItem("news")
+//     console.log(JSON.parse(local));    
+// })();
+
+// module.exports = { localStorage }
