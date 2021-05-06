1b1c97f197c00e90b159ef91f28760d41b784402
Robert <robertwattboolsen@gmail.com>
2021-05-06 15:19:52 +0200

    Script JS: Deleted test code

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,5 @@
 console.log('src/script.js is loaded');
 
-fetchNews("https://rss.nytimes.com/services/xml/rss/nyt/Europe.xml")
-    .then(response => {
-        console.log(response);
-    })
 
 
 
