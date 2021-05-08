9648aa419a9d86d636bbd5b6fc3f06864cf9d7de
Robert <robertwattboolsen@gmail.com>
2021-05-08 21:48:49 +0200

    localstorage JS: create module that saves, deletes and gets LS articles

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -1,9 +1,43 @@
-// let localStorage = (() => {
-//     let newsStr = JSON.stringify(news)
 
-//     localStorage.setItem("news", newsStr)
-//     let local = localStorage.getItem("news")
-//     console.log(JSON.parse(local));    
-// })();
+const articleLS = (() => {
+    let localSavedArticles = JSON.parse(localStorage.getItem("savedArticles"))
+    let savedArticles = localSavedArticles ? localSavedArticles : []
 
-// module.exports = { localStorage }
+    function createArticleID(category) {
+        const randomNumber = Math.random().toString().slice(2, 8)
+        const categoryFirstLetter = category.slice(0, 1)
+        return categoryFirstLetter + randomNumber
+    }
+
+    return {
+        save: (category, cardContent) => {
+            const linkUrl = cardContent.querySelector('.card-content__link').getAttribute('href')
+            const imgSrc = cardContent.querySelector('.card-content__img').getAttribute('src')
+            const heading = cardContent.querySelector('.card-content__heading').innerText
+            const desc = cardContent.querySelector('.card-content__description').innerText
+            category = category.innerText.toLowerCase()
+
+            const article = {
+                link: linkUrl,
+                img: imgSrc,
+                heading: heading,
+                description: desc,
+                category: category,
+                id: createArticleID(category)
+            }
+
+            savedArticles.push(article)
+            localStorage.setItem('savedArticles', JSON.stringify(savedArticles))
+        },
+        delete: (id) => {
+            const articles = JSON.parse(localStorage.getItem("savedArticles"))
+            const filteredArticles = articles.filter(article => article.id != id)
+
+            localStorage.setItem('savedArticles', JSON.stringify(filteredArticles))
+        },
+        get: () => JSON.parse(localStorage.getItem("savedArticles"))
+    }
+})();
+
+
+
