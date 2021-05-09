687513f7794fee57a96fe8bc4d5abebb71d93700
Robert <robertwattboolsen@gmail.com>
2021-05-09 13:42:14 +0200

    index JS: adds card to HTML

--- src/js/index.js
+++ src/js/index.js
@@ -9,16 +9,51 @@ if (window.location.pathname === "/") {
         if (previousPage.includes("settings")) wrapper.classList.add('slide-in-left')
     })();
 
-    (function categoryBtnListener() {
-        document.addEventListener('click', e => {
+    (function addCardToHTML() {
+        const wrapper = document.querySelector('.wrapper')
+        const deletedCategories = categoriesLS.get()
+        const Allcategories = categoryOrder.get()
 
+        if (deletedCategories.length === 0) {
+            Allcategories.forEach(category => {
+                const cardSection = document.createElement('section')
+                cardSection.classList.add('.card')
+                cardSection.innerHTML = `
+                <div class="card-header">
+                    <div class="card-header__circle">
+                        <i class="${getCategoryLogo(category)} card-header__logo"></i>
+                    </div>
+                    <h1 class="card-header__title">${category}</h1>
+                    <div class="card-header__icon-container toggleContent">
+                        <i class="fas fa-chevron-right card-header__icon"></i>    
+                    </div>
+                </div>
+                `
+                wrapper.append(cardSection)
+            })
+        }
+
+
+        categoryBtnListener()
+    })()
+
+    function getCategoryLogo(categoryName) {
+        if (categoryName === "europe") return "fas fa-globe-europe"
+        if (categoryName === "health") return "fas fa-heartbeat"
+        if (categoryName === "sports") return "fas fa-futbol"
+        if (categoryName === "business") return "fas fa-briefcase"
+        if (categoryName === "technology") return "fas fa-microchip"
+    }
+
+    function categoryBtnListener() {
+        document.addEventListener('click', e => {
             // ARROW BTNS
             if (e.target.classList.contains("toggleContent")) {
-                const cardSection = e.target.closest('.card')
+                const cardSection = e.target.parentElement.parentElement
                 const arrowIcon = e.target.children[0]
                 const category = cardSection.querySelector('.card-header__title').innerText
                 const newsArticles = cardSection.querySelectorAll('.card-content')
-
+                
                 if (arrowIcon.style.transform === '') {
                     if (cardSection.children.length == 1) {
                         getNYTArticles(category, cardSection, 'save')
@@ -32,26 +67,6 @@ if (window.location.pathname === "/") {
                     newsArticles.forEach(article => article.style.display = "none")
                 }
             }
-        })        
-    })();
-
-    (function addCardToHTML() {
-        const wrapper = document.querySelector('.wrapper')
-        const deletedCategories = categoriesLS.get()
-        const Allcategories = categoryOrder.get()
-
-        if (deletedCategories.length === 0) {
-            Allcategories.forEach(category => {
-                console.log(category);
-                const cardSection = document.createElement('section')
-
-            })
-            
-            
-        }
-        else {
-
-        }
-    })()
+        })
+    }
 }
-
