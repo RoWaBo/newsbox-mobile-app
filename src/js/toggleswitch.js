974dda87c5c478e9c8b15bb807fccb6f48ec080a
Robert <robertwattboolsen@gmail.com>
2021-05-09 10:24:24 +0200

    JS: created sync switched to LS and add and remove categories toLS

--- src/js/toggleswitch.js
+++ src/js/toggleswitch.js
@@ -1,10 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggle = document.querySelector('#toggle');
 
-    if (toggle) {
-        toggle.addEventListener('click', () => {
-            console.log('toggleSlider is: '+toggle.checked);
-            localStorage.setItem("toggleSwitch", toggle.checked);
-        })
+if (window.location.pathname === "/settings/") {
+
+    const localDeletedCategories = JSON.parse(localStorage.getItem("deletedCategories"))
+    let deletedCategories = localDeletedCategories ? localDeletedCategories : []
+
+    const toggleSwitches = document.querySelectorAll('.switch__check');
+    toggleSwitches.forEach(toggleSwitch => {
+        toggleSwitch.addEventListener('click', saveSwitchStatusToLS)
+
+        const categoryName = toggleSwitch.id.replace('toggle', '').toLowerCase()
+
+        if (deletedCategories.includes(categoryName)) {
+            toggleSwitch.checked = false    
+        } 
+    })
+
+    function saveSwitchStatusToLS(e) {
+        const localDeletedCategories = JSON.parse(localStorage.getItem("deletedCategories"))
+        let deletedCategories = localDeletedCategories ? localDeletedCategories : []
+        const switchStatus = e.target.checked
+        const categoryName = e.target.id.replace('toggle', '').toLowerCase()
+
+        if (!switchStatus && !deletedCategories.includes(categoryName)) {
+            deletedCategories.push(categoryName)
+        }
+        else if (switchStatus && deletedCategories.includes(categoryName)) {
+            deletedCategories.splice(deletedCategories.indexOf(categoryName), 1)
+        }
+
+        localStorage.setItem('deletedCategories', JSON.stringify(deletedCategories))
+        
+        if (localDeletedCategories) {
+            if (localDeletedCategories.length === 0) localStorage.removeItem('deletedCategories')    
+        } 
     }
-});
+
+    // const categoriesLS = (() => {
+
+    // })()
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
