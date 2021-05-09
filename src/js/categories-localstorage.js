dd8803987a32ac8b3718c3415ca54e6d6f2d8829
Robert <robertwattboolsen@gmail.com>
2021-05-09 20:20:18 +0200

    Moved category order code to categories localstorage

--- src/js/categories-localstorage.js
+++ src/js/categories-localstorage.js
@@ -19,3 +19,20 @@ const categoriesLS = (() => {
         }
     }
 })();
+
+const categoryOrder = (() => {
+    const defaultCategories = ["europe","health", "sports", "business", "technology"]
+    let allCategories = [];
+    
+    return {
+        add: categoryName => {
+            allCategories.push(categoryName)
+            localStorage.setItem('categoryOrder', JSON.stringify(allCategories))    
+        },
+        get: () => {
+            const localCategoryOrder = JSON.parse(localStorage.getItem("categoryOrder"))
+            let categoryOrder = localCategoryOrder ? localCategoryOrder : defaultCategories
+            return categoryOrder    
+        } 
+    }
+})();
