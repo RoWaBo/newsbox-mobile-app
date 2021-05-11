d4acb1ce50efc6f14926e2167a4d71cd2241c68a
Robert <robertwattboolsen@gmail.com>
2021-05-11 09:05:09 +0200

    JS + SCSS: testing different ways to setup darkmode

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,6 +2,21 @@
 if (window.location.pathname === "/settings/") {
 
     const toggleSwitches = document.querySelectorAll('.switch__check');
+    const themeBtn = document.querySelector('.toggle-theme-button');
+
+    
+    themeBtn.addEventListener('click', () => {
+        const page = document.querySelector('html')
+        
+       if (page.style.length === 0) {
+        page.style.setProperty('--cBackground', '#2c2c2c')
+        page.style.setProperty('--cFontDark', '#fdfdfd')           
+       } else {
+           page.removeAttribute('style')
+       }
+
+    })
+
 
     toggleSwitches.forEach(toggleSwitch => {
         toggleSwitch.addEventListener('click', e => categoriesLS.toggle(e))
