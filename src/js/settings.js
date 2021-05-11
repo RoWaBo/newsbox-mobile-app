be929cdd78a88f31e6e8253d5873f3d3dfbdca7a
Robert <robertwattboolsen@gmail.com>
2021-05-11 12:57:14 +0200

    Moved sync and save them to LS to it's own file

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,19 +4,19 @@ if (window.location.pathname === "/settings/") {
     const toggleSwitches = document.querySelectorAll('.switch__check');
     const themeBtn = document.querySelector('.toggle-theme-button');
 
-    
-    themeBtn.addEventListener('click', () => {
+    themeBtn.addEventListener('click', e => toggleTheme("light", "dark"))
+
+    function toggleTheme(primary, secondary) {
         const page = document.querySelector('html')
-        
-       if (page.style.length === 0) {
-        page.style.setProperty('--cBackground', '#2c2c2c')
-        page.style.setProperty('--cFontDark', '#fdfdfd')           
-       } else {
-           page.removeAttribute('style')
-       }
 
-    })
-
+        if (page.getAttribute('data-theme') === primary) {
+            page.setAttribute('data-theme', secondary)
+            saveThemeToLS(secondary)
+        } else {
+            page.setAttribute('data-theme', primary)
+            saveThemeToLS(primary)
+        }
+    }
 
     toggleSwitches.forEach(toggleSwitch => {
         toggleSwitch.addEventListener('click', e => categoriesLS.toggle(e))
@@ -29,20 +29,4 @@ if (window.location.pathname === "/settings/") {
 
         categoryOrder.add(categoryName)
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
